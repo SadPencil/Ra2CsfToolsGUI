e5b781275ecb8805cb8a44a7f3c47cc097f04a83
Sad Pencil <sadpencil@outlook.com>
2022-09-03 05:27:45 +0800

    simplify the text in home tab

---
+++ Ra2CsfToolsGUI/BooleanToVisibilityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Ra2CsfToolsGUI
+{
+    // https://stackoverflow.com/a/5182660
+    public sealed class BooleanToVisibilityConverter : BooleanConverter<Visibility>
+    {
+        public BooleanToVisibilityConverter() :
+            base(Visibility.Visible, Visibility.Collapsed)
+        { }
+    }
+}
