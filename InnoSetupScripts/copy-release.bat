1b464aeaaa9f52bd89edc3cfc5c774ef0b57fb01
SadPencil <sadpencil@outlook.com>
2025-08-06 19:43:43 +0800

    Update copy-release.bat

--- InnoSetupScripts/copy-release.bat
+++ InnoSetupScripts/copy-release.bat
@@ -1,7 +1,7 @@
 @echo on
 rd /q /s bin
 mkdir bin
-copy ..\Ra2CsfToolsGUI\bin\Release\*.exe bin\
-copy ..\Ra2CsfToolsGUI\bin\Release\*.exe.config bin\
-copy ..\Ra2CsfToolsGUI\bin\Release\*.dll bin\
+copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.exe bin\
+copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.exe.config bin\
+copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.dll bin\
 pause
