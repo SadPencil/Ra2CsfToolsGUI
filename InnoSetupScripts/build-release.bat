41fb3601c04820aa8c9ff75d21d18bf5d209f9c7
SadPencil <sadpencil@outlook.com>
2025-08-18 18:34:25 +0800

    Update build-release.bat file

--- InnoSetupScripts/build-release.bat
+++ InnoSetupScripts/build-release.bat
@@ -1,10 +1,15 @@
 @echo on
+
+rd /q /s ..\Ra2CsfToolsGUI\bin\Release\net48\
+dotnet build -c Release ..\Ra2CsfToolsGUI\Ra2CsfToolsGUI.csproj
+
 rd /q /s bin
 mkdir bin
+
 copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.exe bin\
 copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.exe.config bin\
 copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.dll bin\
 copy ..\Ra2CsfToolsGUI\bin\Release\net48\*.pdb bin\
 mkdir bin\zh-Hans
-copy ..\Ra2CsfToolsGUI\bin\Release\net48\zh-Hans\*.dll bin\zh-Hans
+copy ..\Ra2CsfToolsGUI\bin\Release\net48\zh-Hans\*.dll bin\zh-Hans\
 pause
